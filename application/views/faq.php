09b5b01020a123a1fc2b935360686cbbc841dace
root <root@localhost.localdomain>
2017-06-02 23:35:05 +0530

    raleway removal

--- application/views/faq.php
+++ application/views/faq.php
@@ -7,7 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?php echo base_url('css/w3.css') ?>">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway"> -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="<?php echo base_url('images/slides/my-slider.css') ?>"/>
 <script src="<?php echo base_url('images/slides/ism-2.2.min.js') ?>"></script>
