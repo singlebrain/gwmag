b12d755fedbcc2ecbccc4bce61a59cc4d373d488
just-sm1 <jamesmathews.mec@gmail.com>
2017-06-02 17:34:04 +0530

    no-dot

--- application/views/home.php
+++ application/views/home.php
@@ -13,7 +13,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <script src="<?php echo base_url('images/slides/ism-2.2.min.js') ?>"></script>
 <style>
 .mySlides {display:none}
-
+w3-left, .w3-right, .w3-badge {cursor:pointer}
+.w3-badge {height:13px;width:13px;padding: ;}
 body {font-family: "Verdana":sans-serif;  background-image: url("<?php echo base_url('images/bg.jpg') ?>");}
 h1,h2,h3,h4,h5,h6 {font-family: "Verdana":sans-serif;}
 </style>
@@ -79,9 +80,9 @@ h1,h2,h3,h4,h5,h6 {font-family: "Verdana":sans-serif;}
   <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
     <div class="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
     <div class="w3-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
-    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
+    <!-- <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
     <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
-    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
+    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span> -->
   </div>
 </div> 
 <script>
@@ -116,17 +117,17 @@ function currentDiv(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
+  // var dots = document.getElementsByClassName("demo");
   if (n > x.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" w3-orange", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-orange";
+  // for (i = 0; i < dots.length; i++) {
+  //    dots[i].className = dots[i].className.replace(" w3-orange", "");
+  // }
+   x[slideIndex-1].style.display = "block";  
+  // dots[slideIndex-1].className += " w3-orange";
 }
 </script>
 <!-- insert here -->
