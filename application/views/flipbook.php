0189f35122c4035f529576d9cc407df32de3475e
root <root@localhost.localdomain>
2017-06-06 14:44:26 +0530

    document changed to d

--- application/views/flipbook.php
+++ application/views/flipbook.php
@@ -34,15 +34,16 @@
 <script src="files/search/book_config.js"></script>
 <link rel="stylesheet" href="style/template.css" />
 <script type="text/javascript">
+var d=document;
 	/*var _gaq = _gaq || [];
 	_gaq.push(['_setAccount', 'UA-00000000-0']);
 	_gaq.push(['_trackPageview']);
 	(function() {
-		var ga = document.createElement('script');
+		var ga = d.createElement('script');
 		ga.type = 'text/javascript';
 		ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0];
+		ga.src = ('https:' == d.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = d.getElementsByTagName('script')[0];
 		s.parentNode.insertBefore(ga, s);
 	})();*/
 		
