40567e7c1542e28f4647373274aef82287ac382b
root <root@localhost.localdomain>
2017-06-14 21:10:08 +0530

    sample fix

--- application/views/sample.php
+++ application/views/sample.php
@@ -17,7 +17,7 @@ $index = $this->session->userdata('index');
 <!-- <script src="<?php// echo base_url('images/slides/ism-2.2.min.js') ?>"></script>
  -->
  <style>
-body {background-image: url("<?php echo base_url('images/bg.jpg') ?>");}
+body {}
 .navi {
     border: solid black;
     border-width: 0 3px 3px 0;
@@ -38,66 +38,8 @@ body {background-image: url("<?php echo base_url('images/bg.jpg') ?>");}
     -webkit-transform: rotate(135deg);
 }
 </style>
-<body>
-
-<!-- Sidebar/menu -->
-<nav class="w3-sidebar w3-collapse w3-animate-left" style=" background-color:#f2f2f2; z-index:3;width:200px;" id="mySidebar"><br>
-  <div class="w3-container">
-    <a href="#" onclick="w3_close()" class="w3-hide-large w3-right w3-padding w3-hover-grey" title="close menu">
-      <i class="fa fa-remove"></i>
-    </a>
-    <!-- <img src="/w3images/avatar_g2.jpg" style="width:45%;" class="w3-round"><br><br> -->
-    <!-- <h4><b>GIANT WHEEL</b></h4>
-     --><!-- <p class="w3-text-grey">Template by W3.CSS</p>
-  </div> -->
-  <div class="w3-bar-block">
-    <a href="javascript:void(0)" onclick="window.location='loadhome'" class="w3-bar-item w3-button w3-padding w3-text-teal w3-hover-blue"><i class="fa fa-th-large fa fa-home w3-margin-right"></i>HOME</a> 
-    <a href="javascript:void(0)" onclick="window.location='loadabout'" class="w3-bar-item w3-button w3-padding w3-hover-blue"><i class="fa fa-user fa-fw w3-margin-right"></i>ABOUT</a> 
-    <a href="javascript:void(0)" onclick="window.location='loadsayso'" class="w3-bar-item w3-button w3-padding w3-hover-blue"><i class="fa fa-user fa fa-comment w3-margin-right"></i>SAY SO</a>
-    <a href="javascript:void(0)" onclick="window.location='loadfaq'" class="w3-bar-item w3-button w3-padding w3-hover-blue"><i class="fa fa-user fa fa-question-circle w3-margin-right"></i>FAQ</a>
-    <a href="javascript:void(0)" onclick="window.location='loadcontact'" class="w3-bar-item w3-button w3-padding w3-hover-blue"><i class="fa fa-envelope fa-fw w3-margin-right"></i>CONTACT</a>
-    <a href="javascript:void(0)" onclick="window.location='loadjoinus'" class="w3-bar-item w3-button w3-padding w3-hover-blue"><i class="fa fa-users fa-fw w3-margin-right"></i>JOIN US</a>
-  </div>
-  <div class="w3-panel w3-large">
-    <i class="fa fa-facebook-official w3-hover-opacity"></i>
-    <i class="fa fa-instagram w3-hover-opacity"></i>
-    <i class="fa fa-snapchat w3-hover-opacity"></i>
-    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-    <i class="fa fa-twitter w3-hover-opacity"></i>
-    <i class="fa fa-linkedin w3-hover-opacity"></i>
-  </div>
-</nav>
 
 <!-- Overlay effect when opening sidebar on small screens -->
-<div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay">
-</div>
-
-<!-- !PAGE CONTENT! -->
-<div class="w3-main" style="margin-left:200px">
-
-  <!-- Header -->
-  <header class=" w3-top" id="portfolio" style=" background-color:#f2f2f2; padding-top:1%; padding-bottom:1% ; opacity: 0.9">
-   <!-- opacity -->
-    <!-- <a href="#"><img src="/w3images/avatar_g2.jpg" style="width:1200px;" class="w3-circle w3-right w3-margin w3-hide-large w3-hover-opacity"></a> -->
-    <div class="w3-cell-row">
-      <span class=" w3-left w3-button w3-hide-large w3-large w3-hover-text-grey" onclick="w3_open()" ><i class="fa fa-bars"></i></span>
-    <!-- <div class="w3-container">
-    <h1><b>My Portfolio</b></h1> -->
-    <!-- <div class="w3-section w3-bottombar w3-padding-16"> -->
-      <!-- <button class="w3-button w3-black">ALL</button> -->
-      <button class=" w3-cell w3-button w3-container " onclick="window.location='loadsample'"><i class="fa fa-book w3-margin-right"></i>READ SAMPLE</button>
-      <button class=" w3-cell w3-button w3-container " onclick="window.location='loadtrial'"><i class="fa fa-clock-o w3-margin-right"></i>TRIAL</button>
-      <button class=" w3-cell w3-button w3-container " onclick="window.location='loadgift'"><i class="fa fa-gift w3-margin-right"></i>GIFT</button>
-      <button  onclick="$('#id01').show();" class=" w3-cell w3-button w3-container "><i class="fa fa-user-circle w3-margin-right"></i>LOG IN/SIGN UP</button>
-    </div>
-    <!-- </div> -->
-  </header>
-<!-- slider -->
-<!-- <div class="w3-content w3-display-container" style="max-width:100%; " >
-</div>  -->
-
-<!-- insert here -->
-
 <script>
 $(document).ready(function(){
   $("#page").on("swipeleft",function(){
